4dabe0296a73384288237f16d52fa3b2b383e4b7
Horserio <a01049149446@gmail.com>
2023-10-11 16:51:33 +0900

    미션 페이지 구조 및 미션 데이터

---
+++ modules/mission_data.py
@@ -0,0 +1,18 @@
+weekly_mission = {
+    "mission1": "손인사하기",
+    "mission2": "How's your jungle life? 물어보기",
+    "mission3": "커피 자리에 몰래 놔두기",
+    "mission4": "조그만한 간식 건내주기",
+    "mission5": "함께 화이팅 외쳐주기",
+    "mission6": "어느 지역에서 왔는지 물어보기",
+    "mission7": "SNS 물어보기",
+    "mission8": "함께 학식 먹자고 물어보기",
+    "mission9": "함께 5분간 산책하자고 물어보기",
+    "mission10": "모르는 문제 알려주기",
+    "mission11": "함께 운동하기",
+    "mission12": "'힘내세요' 쪽지 전달하기",
+    "mission13": "함께 오르막길 오르기",
+    "mission14": "무거운 짐 들어주기",
+    "mission15": "고민 들어주기",
+    "mission16": "따뜻한 아침인사 건내기",
+}
