bc16a4c31b0acb227400a7da4994e485e3720390
Horserio <a01049149446@gmail.com>
2023-10-12 16:09:08 +0900

    미션 메세지 기능

--- modules/mission_complete.py
+++ modules/mission_complete.py
@@ -12,7 +12,8 @@ mission_complete_bp = Blueprint('mission_complete', __name__)
 def mission_complete_fun():
     all_messages = list(db.user.find({'username': "마찬옥"}, {
         'message1': True, 'message2': True, 'message3': True, 'message4': True}))[0]
+    person_i_got_help = db.user.find_one({'username': "강철구"})
 
-    print(all_messages)
+    print(all_messages, person_i_got_help)
 
-    return render_template('final_complete.html', message=all_messages)
+    return render_template('final_complete.html', message=all_messages, person_i_got_help=person_i_got_help)
